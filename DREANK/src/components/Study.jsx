32c11fca59a1a0a6b5e950da6f0820f1d87d6a76
juhye87 <78808933+juhye87@users.noreply.github.com>
2024-06-13 08:35:10 +0900

    에러 x

--- DREANK/src/components/Study.jsx
+++ DREANK/src/components/Study.jsx
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-/* eslint-disable */
-=======
 import React, { useEffect, useState } from "react";
->>>>>>> 4bf29b44bcda7aedc73c3e286014ab943f8f7f83
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Typography from "@mui/joy/Typography";
 import Card from "@mui/joy/Card";
-<<<<<<< HEAD
-import Divider from "@mui/joy/Divider";
-
-// Sample data for studies
-const studies = [
-  {
-    name: "Data Science Study Group",
-    field: "Computer Science",
-    members: 12,
-  },
-  {
-    name: "History Enthusiasts",
-    field: "Humanities",
-    members: 8,
-  },
-];
-=======
 import instance from "../shared/Request"
->>>>>>> 4bf29b44bcda7aedc73c3e286014ab943f8f7f83
 
 export default function Study() {
   const [studies, setStudies] = useState([]);
@@ -62,34 +40,6 @@ export default function Study() {
         가입한 스터디 관리
       </Typography>
 
-<<<<<<< HEAD
-      {studies.map((study, index) => (
-        <Card
-          key={index}
-          sx={{
-            mb: 2,
-            p: 3,
-            boxShadow: 2,
-            border: "1px solid #e0e0e0",
-            width: "700px",
-            borderRadius: "16px",
-          }}
-        >
-          <Typography variant="h6" sx={{ fontWeight: "bold", mb: 1 }}>
-            {study.name}
-          </Typography>
-          <Typography sx={{ fontSize: 14, color: "#666" }}>
-            #{study.field}
-          </Typography>
-          <Typography sx={{ fontSize: 14, color: "#666", mb: 2 }}>
-            회원수: {study.members}명
-          </Typography>
-          <Button variant="plane" sx={{ width: "100%" }}>
-            탈퇴하기
-          </Button>
-        </Card>
-      ))}
-=======
       {studies.length > 0 ? (
         studies.map((study, index) => (
           <Card
@@ -120,7 +70,6 @@ export default function Study() {
       ) : (
         <Typography>가입한 스터디가 없습니다.</Typography>
       )}
->>>>>>> 4bf29b44bcda7aedc73c3e286014ab943f8f7f83
     </Box>
   );
 }
