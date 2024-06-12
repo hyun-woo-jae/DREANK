fe954807bb6a4587a969b02abe40ca6a896f5f4f
hdsjjw <bisha0821@gmail.com>
2024-06-13 02:44:08 +0900

    ss

--- DREANK/src/pages/board/PostDetail.jsx
+++ DREANK/src/pages/board/PostDetail.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 // import React,
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 // import CommentList from "./CommentList";
-=======
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
->>>>>>> 5f4f9e61be9a332f812fe58b849a1f4066a0eeea
 
 function PostDetail() {
   const { id } = useParams();
@@ -23,10 +18,6 @@ function PostDetail() {
     <div className="PostDetail">
       <h1>{post.title}</h1>
       <p>{post.content}</p>
-<<<<<<< HEAD
-      {/* <CommentList postId={id} /> */}
-=======
->>>>>>> 5f4f9e61be9a332f812fe58b849a1f4066a0eeea
     </div>
   );
 }
--- DREANK/src/pages/study/Study.jsx
+++ DREANK/src/pages/study/Study.jsx
@@ -4,26 +4,6 @@ import "./Study.css";
 
 const Study = ({ post }) => {
   return (
-<<<<<<< HEAD
-    <div className="study-item">
-      <h3>{post.name}</h3>
-      <p>{post.introduction}</p>
-      <p>
-        <strong>Number of Recruits:</strong> {post.num_recruit}
-      </p>
-      <p>
-        <strong>Start Time:</strong> {post.start_time}
-      </p>
-      <p>
-        <strong>End Time:</strong> {post.end_time}
-      </p>
-      <p>
-        <strong>Day:</strong> {post.day}
-      </p>
-      <p>
-        <strong>Tag:</strong> {post.tag}
-      </p>
-=======
     <div className='study-container'>
       <div className='study-title'>{post.name}</div>
       <div className='study-info'>{post.introduction}</div>
@@ -32,7 +12,6 @@ const Study = ({ post }) => {
       <div className='study-info'>종료 시간: {post.end_time}</div>
       <div className='study-info'>요일: {post.day}</div>
       <div className='study-info'>태그: {post.tag}</div>
->>>>>>> 75a4e348de9da9d9146cc3cfc086b9baa3c4686a
     </div>
   );
 };
@@ -43,13 +22,8 @@ Study.propTypes = {
     name: PropTypes.string.isRequired,
     introduction: PropTypes.string.isRequired,
     num_recruit: PropTypes.number.isRequired,
-<<<<<<< HEAD
-    start_time: PropTypes.string.isRequired, // Assuming LocalTime is converted to string
-    end_time: PropTypes.string.isRequired, // Assuming LocalTime is converted to string
-=======
     start_time: PropTypes.string.isRequired,
     end_time: PropTypes.string.isRequired,
->>>>>>> 75a4e348de9da9d9146cc3cfc086b9baa3c4686a
     day: PropTypes.string.isRequired,
     tag: PropTypes.string.isRequired,
   }).isRequired,
