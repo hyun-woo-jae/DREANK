8a1c09f90dcf9fce3048554e789a361ca7936217
hdsjjw <bisha0821@gmail.com>
2024-06-12 22:29:11 +0900

    ss

--- DREANK/src/pages/study/Study.jsx
+++ DREANK/src/pages/study/Study.jsx
@@ -5,15 +5,27 @@ import './Study.css';
 const Study = ({ post }) => {
   return (
     <div className='study-item'>
-      <h3>{post.title}</h3>
-      <p>{post.description}</p>
-      {/* 추가 정보가 필요한 경우 여기에 추가 */}
+      <h3>{post.name}</h3>
+      <p>{post.introduction}</p>
+      <p><strong>Number of Recruits:</strong> {post.num_recruit}</p>
+      <p><strong>Start Time:</strong> {post.start_time}</p>
+      <p><strong>End Time:</strong> {post.end_time}</p>
+      <p><strong>Day:</strong> {post.day}</p>
+      <p><strong>Tag:</strong> {post.tag}</p>
     </div>
   );
 };
 
 Study.propTypes = {
-  post: PropTypes.object.isRequired,
+  post: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    introduction: PropTypes.string.isRequired,
+    num_recruit: PropTypes.number.isRequired,
+    start_time: PropTypes.string.isRequired, // Assuming LocalTime is converted to string
+    end_time: PropTypes.string.isRequired,   // Assuming LocalTime is converted to string
+    day: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Study;
