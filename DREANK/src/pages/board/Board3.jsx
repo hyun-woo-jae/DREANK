285b402a42321783900fed5cf9745d34b7bffec6
hyunwoo <hwjae1004@naver.com>
2024-06-13 02:15:02 +0900

    게시판쫌수정

--- DREANK/src/pages/board/Board3.jsx
+++ DREANK/src/pages/board/Board3.jsx
@@ -11,11 +11,11 @@ const Board = ({ pagelink, position, title, explain }) => {
       <div className="board-title">{title}</div>
       <div className="board-ex">{explain}</div>
       <br />
-      <div className="board-writing">강남/토익스터디</div>
-      <div className="board-writing">삼성 코테준비 같이해요!!</div>
-      <div className="board-writing">네이버 최종면접 준비 같이하실분?</div>
-      <div className="board-writing">오픽 연습 같이해요</div>
-      <div className="board-writing">토익800점목표</div>
+      <div className="board-writing">삼성 최종 합격했습니다.</div>
+      <div className="board-writing">2주만에 토익 300점에서 700점된 썰</div>
+      <div className="board-writing">자소서 첨삭해드림~~</div>
+      <div className="board-writing">독학으로 ncs 합격</div>
+      <div className="board-writing">면접 안 떠는 꿀팁</div>
       <Link to={pagelink}>
         <button type="button" className="detail-button">
           더보기
